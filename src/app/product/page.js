ad212c3f7b2c996d2c338b876921775af182c873
Yaseenjabir <yaseenkhan1235476@gmail.com>
2024-09-17 14:38:54 +0500

    init

--- src/app/product/page.js
+++ src/app/product/page.js
@@ -2,7 +2,8 @@ import Link from "next/link";
 import Button from "../../../components/button";
 
 async function getData() {
-  let response = await fetch("http://localhost:3000/api/products");
+  const apiUrl = process.env.API_URL;
+  let response = await fetch(`${apiUrl}/api/products`);
   response = await response.json();
   console.log(response);
   return response;
@@ -16,16 +17,17 @@ const Page = async () => {
       <h1>Hello world</h1>
       <div>
         {ans.map((item) => (
-          <>
-            <div style={{ border: "1px solid red", margin: "20px 0px" }}>
-              <h3>Name : {item.name}</h3>
-              <h3>Color : {item.color}</h3>
-              <h3>Company :{item.company}</h3>
-              <h3>Price : {item.price}</h3>
-              <Link href={`/product/${item._id}`}>Update</Link>
-              <Button id={item._id} />
-            </div>
-          </>
+          <div
+            key={item._id}
+            style={{ border: "1px solid red", margin: "20px 0px" }}
+          >
+            <h3>Name : {item.name}</h3>
+            <h3>Color : {item.color}</h3>
+            <h3>Company : {item.company}</h3>
+            <h3>Price : {item.price}</h3>
+            <Link href={`/product/${item._id}`}>Update</Link>
+            <Button id={item._id} />
+          </div>
         ))}
       </div>
     </>
